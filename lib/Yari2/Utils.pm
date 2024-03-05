574f3b7af9631390b49e53a6430b52b68d491afd
moldyelk <33961574+moldyelk@users.noreply.github.com>
2024-03-05 09:48:14 +0900

    Create Utils.pm

---
+++ lib/Yari2/Utils.pm
@@ -0,0 +1,35 @@
+#=================================================================================
+#=================================================================================
+package Yari2::Utils;
+
+use 5.010;
+use strict;
+use warnings FATAL => 'all';
+
+use parent qw(Exporter);
+use Yari2::Parser qw/parser $vlog_parser %param_table/;
+use Data::Dump qw/dump pp dd/;
+use POSIX;
+
+=head1 NAME
+Yari2::Utils - Yari Utilities
+
+=head1 VERSION
+
+Version 1.12
+
+=cut
+
+our $VERSION = '1.12';
+
+
+
+
+
+
+
+
+
+
+
+1;
